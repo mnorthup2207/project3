d223b8dedec1bbcaab39003dae2591e2d95244f0
McCabe Northup <mccabenorthup@gmail.com>
2020-03-04 12:54:04 -0700

    updated map page, needs text and some state updates

--- client/src/components/Animation_Card/index.js
+++ client/src/components/Animation_Card/index.js
@@ -1,33 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
 ////Sprites////
 import Sprite from "../../sprites/getSprite";
 import sprites from "../../sprites/sprites.json";
-
 import "./style.css";
+import { PromiseProvider } from 'mongoose';
 
-const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-});
-
-export default function OutlinedCard() {
-
+const AnimatedCard = (props) => {  
     return (
-        <Sprite charcter={sprites.player.main} />
+        <Sprite character={props[0]} />
     );
 }
 
--- client/src/pages/Map/index.js
+++ client/src/pages/Map/index.js
@@ -1,11 +1,18 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
+
+////Sprites////
+import Sprite from "../../sprites/getSprite";
+import sprites from "../../sprites/sprites.json";
+
 ////Material UI////
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+// import mapPic from "../../images/map-color.png";
+import mapPic1 from "../../images/map-color-ratio.png";
 import "./style.css"
-
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
@@ -16,33 +23,51 @@ const useStyles = makeStyles(theme => ({
         color: theme.palette.text.secondary,
     },
 }));
+const currentMapBoss = [sprites.boss.one, sprites.boss.one, sprites.boss.one];
 
-const DungeonFight = (props) => {
+const MapPage = (props) => {
     const classes = useStyles();
-    useEffect(() => {
-        const canvas = document.getElementById('draw');
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        canvas.addEventListener('mousedown', (e) => {
-            console.log(e);
-
-            // [lastX, lastY] = [e.offsetX, e.offsetY];
-        });
-    })
     return (
-        <Container id="fightContainer" maxWidth="lg">
-            <Grid container spacing={3} className={classes.root}>
-                <Grid item xs>
-                    <Link to="/home"><h2>Home</h2></Link>
+        <>
+            <img id="mapPic" src={mapPic1} alt="map image" />
+            <div className="helpfulDiv"></div>
+            <Container id="fightContainer" maxWidth="lg">
+                <Grid container spacing={3} className={classes.root}>
+                    <Grid item xs>
+                    <Link to="/home">
+                            <Button
+                                color="primary"
+                                size="large"
+                            >
+                                <h1>Home</h1>
+                                <i className="material-icons" style={{ marginLeft: 10 }}>house</i>
+                            </Button>
+                        </Link>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Grid id="shift" container spacing={3} className={classes.root}>
-                <Grid item xs>
-                    <canvas id="draw" width="800" height="800"></canvas>
+                <Grid container
+                    id="shift"
+                    justify="flex-start"
+                    alignItems="center" 
+                    className={classes.root}>
+                    <Grid item xs={6}>
+                        <Sprite character={currentMapBoss[0]} />
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Link to={"/fight"}>
+                        <h3>Unidentified vessel travelling at sub warp speed, bearing 235.7. 
+                            Fluctuations in energy readings from it, Captain. All transporters off. 
+                            A strange set-up, but I’d say the graviton generator is depolarized. 
+                            The dark colourings of the scrapes are the leavings of natural rubber, 
+                            a type of non-conductive sole used by researchers experimenting with electricity. 
+                            The molecules must have been partly de-phased by the anyon beam.
+                            </h3>
+                        </Link>    
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        </>
     );
 };
 
-export default DungeonFight;
+export default MapPage;
