b3e454b1ffd52f1120a9802d7b71273c9008c72a
McCabe Northup <mccabenorthup@gmail.com>
2020-03-08 14:05:29 -0600

    updated and finished creator page

--- client/src/components/Creator_Card/index.js
+++ client/src/components/Creator_Card/index.js
@@ -1,45 +1,28 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
-import CardContent from '@material-ui/core/CardContent';
+import Sprite from '../../sprites/getSprite.js'
+import "./style.css"
 
-const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)',
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-});
 
 const CreatorCard = (props) => {
-    const classes = useStyles();
     console.log(props);
-    const { name, GitHub, FavoriteSpell, creatorImg } = props
+    const { name, GitHub, FavoriteSpell, handle, character, type } = props
     return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Grid container spacing={3}>
-                    <Grid item xs>
-                        <h4>{name}</h4>
-                        <h4><a href={GitHub} target="_blank">GitHub Account: {GitHub}</a></h4>
-                        <h4>Favorite Spell: {FavoriteSpell}</h4>
-                    </Grid>
-                    <Grid item xs>
-                        <img src={creatorImg} alt="Sprite Image" />
-                    </Grid>
-                </Grid>
-            </CardContent>
-        </Card>
+        <Grid container id="creatorContainer" spacing={3}>
+            <Grid item xs>
+                <h4>{name}</h4>
+                <h4><a href={GitHub} target="_blank">GitHub Account: {handle}</a></h4>
+                <h4>Favorite Spell: {FavoriteSpell}</h4>
+            </Grid>
+            <Grid item xs>
+                <div id='boom2'>
+                    <Sprite
+                        character={character}
+                        type={type}
+                    />
+                </div>
+            </Grid>
+        </Grid>
     );
 }
 
