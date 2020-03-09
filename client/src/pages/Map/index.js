598c357fe0191d0a3f92a91f8cdf06160c970236
McCabe Northup <mccabenorthup@gmail.com>
2020-03-09 11:58:37 -0600

    updated map page story line

--- client/src/pages/Map/index.js
+++ client/src/pages/Map/index.js
@@ -15,24 +15,23 @@ import { makeStyles } from '@material-ui/core/styles';
 // import mapPic from "../../images/map-color.png";
 import mapPic1 from "../../images/map-color-ratio.png";
 import "./style.css"
-const useStyles = makeStyles(theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-}));
-const currentMapBoss = ["one", "two", "three"];
+
+const storyLine = [
+        "Nicknamed the one-man Army. Rumor is after one battle thought to be a suicide mission for his Heard, your Opponent walked away from the only survivor from either side. Make no mistake, even though this is your first battle this opponent will not go easy on you.",
+        "An unremorseful warmonger. This anarchist is always the one to start the fire that burns the defeated village. His only mission in life is wandering far and wide looking to start a fight, but more importantly, he always ends them too.",
+        "As the story goes this Monster leads his herd into several civilian villages, murdered and pillaged. Until some of his soldiers talked of mutiny for the unjust murders. The entirety of the army had a tragic accident in a rockslide, All except your next opponent.",
+        "Wants nothing more than to Burn. He leaves a trail of Scorched Earth and Burning corpses on the way to his next atrocity.",
+        "Wants nothing more than to Burn. He leaves a trail of Scorched Earth and Burning corpses on the way to his next atrocity.",
+        "Plans to turn the land into a frozen tundra. He walks from town to town freezing watering holes to the core and causes livestock to die from Hypothermia. You must stop him before his actions cause a mass Famine.",
+];
 
 const MapPage = (props) => {
     const monsters = useSelector(state => state.monsters[0]);
     const { character, type } = useSelector(state => state.monsterSprite);
     const battleNumber = useSelector(state => state.player.battleNumber);
     const dispatch = useDispatch();
-
+    console.log(battleNumber);
+    const story = storyLine[battleNumber]
     useEffect(() => {
         console.log(monsters)
         let newMonster = monsters.filter(monster => monster.order === battleNumber)[0]
@@ -40,13 +39,13 @@ const MapPage = (props) => {
         dispatch(setMonster(newMonster));
         dispatch(setMonsterSprite(newMonster.animation.character, newMonster.animation.type))
     }, [battleNumber])
-    const classes = useStyles();
+    
     return (
         <>
             <img id="mapPic" src={mapPic1} alt="map" />
             <div className="helpfulDiv"></div>
             <Container id="fightContainer" maxWidth="lg">
-                <Grid container spacing={3} className={classes.root}>
+                <Grid container spacing={3} >
                     <Grid item xs>
                         <Link to="/home">
                             <Button
@@ -63,7 +62,7 @@ const MapPage = (props) => {
                     id="shift"
                     justify="flex-start"
                     alignItems="center"
-                    className={classes.root}>
+                    >
                     <Grid item xs={6}>
                         <div id="spriteDiv">
                             <Sprite character={character} type={type} />
@@ -71,13 +70,7 @@ const MapPage = (props) => {
                     </Grid>
                     <Grid item xs={4}>
                         <Link to={"/fight"}>
-                            <h3>Unidentified vessel travelling at sub warp speed, bearing 235.7.
-                                Fluctuations in energy readings from it, Captain. All transporters off.
-                                A strange set-up, but I’d say the graviton generator is depolarized.
-                                The dark colourings of the scrapes are the leavings of natural rubber,
-                                a type of non-conductive sole used by researchers experimenting with electricity.
-                                The molecules must have been partly de-phased by the anyon beam.
-                            </h3>
+                            <h3>{story}</h3>
                         </Link>
                     </Grid>
                 </Grid>
